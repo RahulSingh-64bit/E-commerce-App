6d967f8805b6782868592922312f6abd8e2d6fb8
RahulSingh-64bit <rahulrawat88607@gmail.com>
2025-09-16 22:20:52 +0530

    changes in type.ts

--- src/types.ts
+++ src/types.ts
@@ -31,4 +31,8 @@ export const shippingFormSchema = z.object({
         .min(7, "Phone number must be between 7 and 10 digits!")
         .max(14, "Phone number must be between 7 and 10 digits!")
         .regex(/^\d+$/, "Phone number must contain numbers only!"),
+        address:z.string().min(1, "Address is required!"),
+        city:z.string().min(1, "City is required!"),
 });
+
+export type ShippingFormInputs = z.infer<typeof shippingFormSchema>;
