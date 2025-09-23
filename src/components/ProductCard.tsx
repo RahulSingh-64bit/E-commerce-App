6c82990f3ae82fcf433c49f9f6a7e1b66d9dd4f6
RahulSingh-64bit <rahulrawat88607@gmail.com>
2025-09-23 22:06:52 +0530

    add product interaction component

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -39,7 +39,7 @@ const handleAddToCart = ()=>{
   return (
     <div className="shadow-lg rounded-lg overflow-hidden">
       {/* IMAGE */}
-      <Link href={`/product/${product.id}`}>
+      <Link href={`/products/${product.id}`}>
         <div className="relative aspect-[2/3]">
           <Image
             src={product.images[productTypes.color]}
